97a6ba9c289e418a0cf8be40eb190e0467f2e36a
Fabio Y. Goto <fabio.goto@gmail.com>
2020-04-17 10:04:06 -0300

    Updating website info

---
+++ build/README.md
@@ -0,0 +1,12 @@
+finishthatcode.github.io
+========================
+
+> Website coletivo FinishThatCode.
+
+Este website é uma aplicação criada com `create-react-app`, portanto veja o `README-CRA.md` para mais detalhes (em inglês).
+
+Para maiores detalhes e código-fonte, veja a branch `develop` deste projeto.
+
+----------------------------------------------------------------------
+
+_©2020 FinishThatCode_
--- src/Main.js
+++ src/Main.js
@@ -16,7 +16,24 @@ export default class Main extends Component {
         <div class="ftc-main">
           <img src={Logo} alt={"/>"}/>
           <h1>FINISH THAT CODE</h1>
-          <p>Aqui ninguém vence sozinho. FINISH THAT CODE, NOW.</p>
+
+          <p className={"lead"}>
+            Codar, progredir e ajudar.<br/>
+            Aqui ninguém vence sozinho.<br/>
+            FINISH THAT CODE, NOW.
+          </p>
+
+          <hr/>
+
+          <p>O <strong>FinishThatCode</strong> é um pequeno coletivo criativo, grupo de estudos, discussão e incentivo para desenvolvedores web, cujo foco é o auxílio no desenvolvimento das habilidades e, também, projetos comunitários.</p>
+
+          <p>Alguns dos projetos serão open source e abertos ao público, enquanto outros ficarão limitados para membros. O uso dos projetos pelos membros, porém, é irrestrito. Limitamos o acesso ao público em geral unicamente por questões de organização e manutenção.</p>
+
+          <hr/>
+
+          <p style={{fontWeight: 200}}>
+            &copy;2020 Finish That Code
+          </p>
         </div>
       </>
     );
