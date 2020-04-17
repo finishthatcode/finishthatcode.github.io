b2e294f64e4b786b426371c771771c36b954a884
Fabio Y. Goto <fabio.goto@gmail.com>
2020-04-17 01:47:28 -0300

    Added build

---
+++ build/precache-manifest.d1cf7dd7f76fd25b67403f540e0a2d9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27abab9fdb6dbf4edaa57042359dd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf5b73ff982595dca3a",
+    "url": "/static/css/main.d2c5c7ad.chunk.css"
+  },
+  {
+    "revision": "2a923b182c6936332cf5",
+    "url": "/static/js/2.c95a3e38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c95a3e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf5b73ff982595dca3a",
+    "url": "/static/js/main.87e6b71e.chunk.js"
+  },
+  {
+    "revision": "3b25e8a0ddb5ee813bbd",
+    "url": "/static/js/runtime-main.d7a8dfc1.js"
+  }
+]);
